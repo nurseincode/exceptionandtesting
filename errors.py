2d50b401fe411d951cbce6994b7461bc481103db
https://github.com/nurseincode <nurseincode@gmail.com>
2025-10-09 20:45:36 +1100

    modified try except block

--- errors.py
+++ errors.py
@@ -1,17 +1,37 @@
-def convert_to_integer(value):
-    result = int(value)
-    print(f'Conversion successful! Result: {result}')
+# def convert_to_integer(value):
+#     result = int(value)
+#     print(f'Conversion successful! Result: {result}')
 
-# User input
-# spam = input('Enter a number to convert to integer: ') # Built input() always returns a string
-# print(spam)
-# print(type(spam))
+# # User input
+# # spam = input('Enter a number to convert to integer: ') 
+# # print(spam) 
+# # print(type(spam)) # Builtin input() always returns a string
+
+# # User input
+# spam = input('Enter a number: ')
+# convert_to_integer(spam) # Calling the function explicitly: prints int when input is an int
+# convert_to_integer('50') # Calling the function explicitly with str value: convertes to int
+# print(type(convert_to_integer)) # Class function
+
+# Handling the exception
+def convert_to_integer(value):
+    try:
+        x = 10 / 0
+        result = int(value)
+        print(f'Conversion successful! Result: {result}')
+    except ValueError:
+       print(f'Conversion failed. Please enter a valid integer.')
+    # except: Handles all exceptions, you can see what the error was
+    except Exception as e: # Catches exception + stores the exception object in a variable e
+        print(f'An unexpected error occured {e}')
+# #   except (ValueError, ZeroDivisionError) handling specific multiple errors
+#     except ZeroDivisionError:
+#         print('Divide by zero!')
+    else: # Only runs if no exceptions occurred
+        print(f'Else invoked')
+    finally: # Always runs
+        print(f'Closing any open resources')
 
 # User input
 spam = input('Enter a number: ')
-convert_to_integer(spam) # Calling the function explicitly: prints an integer
-# print(type(spam))
-convert_to_integer('50') # Calling the function explicitly with str value: prints an integer
-print(type(convert_to_integer))
-
-
+convert_to_integer(spam) 
